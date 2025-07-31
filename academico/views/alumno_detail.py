0614807a096da81f94090b8ab7083db1f70fdc00
robertbarac <ryba-07@hotmail.com>
2025-07-31 18:49:12 -0500

    mensajería de cobor en alumno_detail

--- academico/views/alumno_detail.py
+++ academico/views/alumno_detail.py
@@ -1,6 +1,8 @@
 from django.views.generic import DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db.models import Avg
+from django.utils import timezone
+from django.template.loader import render_to_string
 
 from academico.models import Alumno, Asistencia, Nota
 from cartera.models import Deuda
@@ -22,6 +24,37 @@ class AlumnoDetailView(LoginRequiredMixin, DetailView):
         try:
             deuda = alumno.deuda
             cuotas = deuda.cuotas.all()
+            
+            # Agregar mensaje WhatsApp a cada cuota
+            for cuota in cuotas:
+                # Forzar la actualización del estado de la cuota según la fecha actual
+                if cuota.monto_abonado >= cuota.monto:
+                    cuota.estado = "pagada"
+                elif cuota.monto_abonado > 0:
+                    cuota.estado = "pagada_parcial"
+                elif timezone.localtime(timezone.now()).date() > cuota.fecha_vencimiento:
+                    cuota.estado = "vencida"
+                else:
+                    cuota.estado = "emitida"
+                    
+                cuota.dias_atraso = (timezone.localtime(timezone.now()).date() - cuota.fecha_vencimiento).days if cuota.fecha_vencimiento < timezone.localtime(timezone.now()).date() else 0
+                
+                # Renderizar el mensaje con los datos actuales
+                message_context = {
+                    'nombres': alumno.nombres,
+                    'primer_apellido': alumno.primer_apellido,
+                    'fecha_vencimiento': cuota.fecha_vencimiento,
+                    'dias_atraso': cuota.dias_atraso,
+                    'monto': cuota.monto,
+                    'monto_abonado': cuota.monto_abonado,
+                    'saldo_pendiente': cuota.deuda.saldo_pendiente
+                }
+                
+                # Renderizar el template y codificar para URL
+                cuota.whatsapp_message = render_to_string(
+                    'cartera/whatsapp_message_template.txt',
+                    message_context
+                ).replace('\n', '%0A').replace(' ', '%20')
         except Deuda.DoesNotExist:
             deuda = None
             cuotas = []
