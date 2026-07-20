85b0ec2b4b6752c67d8aa01661e2c86b584872c5
Robert Barrios <ryba.9125@gmail.com>
2026-07-20 17:30:55 -0500

    Configura base de datos dinamica con django-environ y postgres

--- preicfes/settings.py
+++ preicfes/settings.py
@@ -100,10 +100,7 @@ WSGI_APPLICATION = 'preicfes.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': env.db('DATABASE_URL', default=f'sqlite:///{os.path.join(BASE_DIR, "db.sqlite3")}')
 }
 
 
